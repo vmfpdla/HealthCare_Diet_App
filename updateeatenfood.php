77860fc2e07eb58861874dae1a51b00751c654e8
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2020-10-09 14:19:52 +0900

    Create updateeatenfood.php

---
+++ updateeatenfood.php
@@ -0,0 +1,19 @@
+<?php
+$require_once('./dbconn.php');
+$user_id=1
+$eaten_id = 1;
+$food_id=1;
+$eaten_time=2;
+$eaten_serving=1;
+$eaten_day = date("Y-m-d",time());
+
+
+
+$sql = "INSERT INTO eatenfood (eaten_id, user_id, food_id, eaten_day, eaten_time, eaten_serving) VALUES ($eaten_id,$user_id,$food_id,$eaten_day,$eaten_time,$eaten_serving)";
+
+
+$conn=query($sql);
+
+$conn->close();
+
+?>
