817de325630ac9eca140279f6e0dda57dbe52be5
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2020-10-09 12:33:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -47,9 +47,8 @@ $user_id = 1; # 1번 가져왔다고 가정
   <!-- 자바스크립트 -->
   <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="../js/nav.js"></script>
-  <script src="../js/camera.js"></script>
-
+  <script src="./js/nav.js"></script>
+ 
 </head>
 
 <body>
