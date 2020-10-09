ca801736374a8137452fb213b51e91843380e066
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2020-10-09 12:58:43 +0900

    Update recommend.php

--- recommend.php
+++ recommend.php
@@ -4,7 +4,7 @@
 	<title></title>
 
 	<link rel="stylesheet" href="./css/jaehyun.css">
-	 <script src="./js/nav.js"></script>
+	
  
 	  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/v4-shims.css">
@@ -12,9 +12,9 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-
+ <script src="./js/nav.js"></script>
 </STYLE>
-<script src="./js/recommend.js">
+<script src="./js/recommend.js"> </script>
 
 </script>
 </head>
