c6d445f3943f42bd585c7069771b38ed99d08430
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2020-10-09 14:34:58 +0900

    Update updateeatenfood.php

--- updateeatenfood.php
+++ updateeatenfood.php
@@ -8,13 +8,14 @@ $eaten_serving=1;
 $eaten_day = date("Y-m-d",time());
 
 
+$sql = "INSERT INTO eatenfood (eaten_id, user_id, food_id, eaten_day, eaten_time, eaten_serving) VALUES ($eaten_id,$user_id,$food_id,$eaten_day,$eaten_time,$eaten_serving)";
 
-$sql = "INSERT INTO eatenfood (eaten_id, user_id, food_id, eaten_day, eaten_time, eaten_serving) VALUES ($eaten_id,$user_id,$food_id,$eaten_day,$eaten_time,$eaten_serving);";
 
-
-$conn=query($sql);
+$conn=mysqli_query($sql);
 
 $conn->close();
+
+header("Location: ./index.php");
 
 ?>
 
