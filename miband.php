044a8cfad035adefe92f9ff42b77b6772489278b
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2020-10-09 12:57:59 +0900

    Update miband.php

--- miband.php
+++ miband.php
@@ -3,8 +3,7 @@
 
 <head>
   
-  <scrip src="./js/nav.js"></script>
-
+ 
 
   <link rel="stylesheet" href="./css/jaehyun.css">
 
@@ -16,6 +15,8 @@
   <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <meta charset="utf-8">
+	 <script src="./js/nav.js"></script>
+
 </head>
 
 <body>
