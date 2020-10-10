312862495ba7e7a0085b802024eccc782d6026b6
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2020-10-10 17:41:19 +0900

    Update test.php

--- test.php
+++ test.php
@@ -43,6 +43,48 @@
     })
   </script>
 
+  <script type="text/javascript">
+
+
+    // 테이블의 Row 클릭시 값 가져오기
+    $("#user-table tr").click(function(){  
+
+      var str = ""
+      var tdArr = new Array();  // 배열 선언
+      
+      // 현재 클릭된 Row(<tr>)
+      var tr = $(this);
+      var td = tr.children();
+      
+      // tr.text()는 클릭된 Row 즉 tr에 있는 모든 값을 가져온다.
+      console.log("클릭한 Row의 모든 데이터 : "+tr.text());
+      
+      // 반복문을 이용해서 배열에 값을 담아 사용할 수 도 있다.
+      td.each(function(i){
+        tdArr.push(td.eq(i).text());
+      });
+      
+      console.log("배열에 담긴 값 : "+tdArr);
+      
+      // td.eq(index)를 통해 값을 가져올 수도 있다.
+      var no = td.eq(0).text();
+      var userid = td.eq(1).text();
+      var name = td.eq(2).text();
+      var email = td.eq(3).text();
+      
+      
+      str +=  " * 클릭된 Row의 td값 = No. : <font color='red'>" + no + "</font>" +
+      ", 아이디 : <font color='red'>" + userid + "</font>" +
+      ", 이름 : <font color='red'>" + name + "</font>" +
+      ", 이메일 : <font color='red'>" + email + "</font>";   
+      
+      $("#ex1_Result1").html(" * 클릭한 Row의 모든 데이터 = " + tr.text());    
+      $("#ex1_Result2").html(str);
+    });
+
+
+  </script>
+
 
 </head>
 
@@ -74,18 +116,20 @@
           header('Content-Type:text/html; charset=UTF-8');
           $user_id=1;
           $jb_conn = mysqli_connect( 'localhost', 'root', 'toor', 'smartpt' );
-          $jb_sql = "SELECT * FROM foodinfo Where user_id='$user_id';";
+          $jb_sql = "SELECT * FROM foodinfo";
           $jb_result = mysqli_query( $jb_conn, $jb_sql );
 
           while( $jb_row = mysqli_fetch_array( $jb_result ) ) {
-          echo '<tr><td>' . $jb_row[ 'food_id' ] . '</td><td>'. $jb_row[ 'food_name' ] . '</td><td>' . $jb_row[ 'food_car' ] . '</td><td>'. $jb_row[ 'food_fat' ] .'</td><td>'. $jb_row[ 'food_pro' ] .'</td><td>'. $jb_row[ 'food_calory' ] .'</td></tr>';
-        }
-        ?>
-      </tbody>
-    </table>
-  </form>
+            echo '<tr><td>' . $jb_row[ 'food_id' ] . '</td><td>'. $jb_row[ 'food_name' ] . '</td><td>' . $jb_row[ 'food_car' ] . '</td><td>'. $jb_row[ 'food_fat' ] .'</td><td>'. $jb_row[ 'food_pro' ] .'</td><td>'. $jb_row[ 'food_calory' ] .'</td></tr>';
+          }
+          ?>
+        </tbody>
+      </table>
+    </form>
 
-
+    <div class="col-lg-12" id="ex1_Result1" ></div> 
+    <div class="col-lg-12" id="ex1_Result2" ></div> 
+  </div>
 
 
 </body>
