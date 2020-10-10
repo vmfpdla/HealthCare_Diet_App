bb5e100145296f1eedb46b78f742931115011d8f
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2020-10-10 17:34:20 +0900

    Update test.php

--- test.php
+++ test.php
@@ -114,13 +114,13 @@ require_once("./dbconn.php");
 
       <tbody>
         <?php
-        header('Content-Type:text/html; charset=UTF-8');
+          header('Content-Type:text/html; charset=UTF-8');
+          $user_id=1;
+          $jb_conn = mysqli_connect( 'localhost', 'root', 'toor', 'smartpt' );
+          $jb_sql = "SELECT * FROM foodinfo Where user_id='$user_id';";
+          $jb_result = mysqli_query( $jb_conn, $jb_sql );
 
-
-        $jb_sql = "SELECT * FROM foodinfo;";
-        $jb_result = mysqli_query( $conn, $jb_sql );
-
-        while( $jb_row = mysqli_fetch_array( $jb_result ) ) {
+          while( $jb_row = mysqli_fetch_array( $jb_result ) ) {
           echo '<tr><td>' . $jb_row[ 'food_id' ] . '</td><td>'. $jb_row[ 'food_name' ] . '</td><td>' . $jb_row[ 'food_car' ] . '</td><td>'. $jb_row[ 'food_fat' ] .'</td><td>'. $jb_row[ 'food_pro' ] .'</td><td>'. $jb_row[ 'food_calory' ] .'</td></tr>';
         }
         ?>
