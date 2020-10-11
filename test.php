76139fac1c50c4800a82f33f94df08a559b52a56
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2020-10-11 17:56:36 +0900

    Update test.php

--- test.php
+++ test.php
@@ -95,6 +95,18 @@
       ?>
     </tbody>
   </table>
+	  
+	  <table>
+  <tr>
+    <td>Filter Name</td>
+    <td>Filter ID</td>
+  </tr>
+  <?php
+  foreach (filter_list() as $id =>$filter) {
+    echo '<tr><td>' . $filter . '</td><td>' . filter_id($filter) . '</td></tr>';
+  }
+?>
+</table>
 
 
 
