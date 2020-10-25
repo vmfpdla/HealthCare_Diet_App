c87a389a71e8f7328fc66a4f5417278ad026ba8c
root <root@capstone.asia-northeast3-a.c.sogading.internal>
2020-10-25 11:12:40 +0000

    diet modify

--- diet.php
+++ diet.php
@@ -18,21 +18,27 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
 $sql1 = "SELECT * FROM diet";
 $result1 = $conn->query($sql1);
 
-
-$count=0;
-if ($result1->num_rows > 0) { // 여러줄 가져오는 경우
+#$arr = array(
+#	array()
+#);	
+$arr= array();
+  if ($result1->num_rows > 0) { // 여러줄 가져오는 경우
 
   while($row = $result1->fetch_assoc()) {
-  $diet_arry[$count]=[ 
-      [
-          $row['diet_id'],$row['diet_grains'],$row['diet_meat'],$row['diet_vet'],$row['diet_else'],$row['diet_calory'];
-      ]
-  ]
-  $count+=1;
-  echo $row['diet_id'] ." 번식단 밥종류:" .$row['diet_grains'] ." 고기종류: ".$row['diet_meat']."채소 종류 ".$row['diet_vet']."기타: ".$row['diet_else']."칼로리 :".$row['diet_calory'];
-  echo nl2br("\n");
-}
+	  array_push($arr,array($row['diet_id'],$row['diet_grains'],$row['diet_meat'],$row['diet_vet'],$row['diet_else'],$row['diet_calory']));
+	  
+	  echo $row['diet_id'] ." 번식단 밥종류:" .$row['diet_grains'] ." 고기종류: ".$row['diet_meat']."채소 종류 ".$row['diet_vet']."기타: ".$row['diet_else']."칼로리 :".$row['diet_calory'];
+	  echo nl2br("\n");
+  }
 } else {
   echo "0 results";
+}
+
+for($i=0;$i<count($arr);$i++){
+	for($j=0;$j<6;$j++){
+		echo $arr[$i][$j];
+		echo nl2br("\n");
+	}
+
 }
 ?>
