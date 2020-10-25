98a0da93a4e9dc08477a96ed5fbcd807260a9940
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2020-10-25 20:17:20 +0900

    Update recommend2.php

--- recommend2.php
+++ recommend2.php
@@ -105,38 +105,42 @@
 			</form>
 			<div id="setKcal" style="display: none;">
 				<?php
-					require_once("./dbconn.php");	
+
+					require_once("./dbconn.php");
 					$user_id = 1; # 1번 가져왔다고 가정
 					$sql = "SELECT * FROM user WHERE user_id='$user_id'";
 					$result = $conn->query($sql);
 
 					if ($result->num_rows > 0) { // 여러줄 가져오는 경우
-					  while($row = $result->fetch_assoc()) {
-    					$user = $row;
+						while($row = $result->fetch_assoc()) {
+   							 $user = $row;
   						}
 					} else {
-  						echo "유저 접속 오류";
+ 						 echo "유저 접속 오류";
 					}
 
 					$sql1 = "SELECT * FROM diet";
 					$result1 = $conn->query($sql1);
-					$count=0;
-					if ($result1->num_rows > 0) { // 여러줄 가져오는 경우
 
- 					 while($row = $result1->fetch_assoc()) {
- 					 $diet_arry[$count]=[ 
-     						[
-         					 $row['diet_id'],$row['diet_grains'],$row['diet_meat'],$row['diet_vet'],$row['diet_else'],$row['diet_calory'];
-      						]
-  						]
-  						$count+=1;
-						echo $row['diet_id'] ." 번식단 밥종류:" .$row['diet_grains'] ." 고기종류: ".$row['diet_meat']."채소 종류 ".$row['diet_vet']."기타: ".$row['diet_else']."칼로리 :".$row['diet_calory'];
-  						echo nl2br("\n");
-						}
+					$arr= array();
+					if ($result1->num_rows > 0) { // 여러줄 가져오는 경우
+					  while($row = $result1->fetch_assoc()) {
+						  array_push($arr,array($row['diet_id'],$row['diet_grains'],$row['diet_meat'],$row['diet_vet'],$row['diet_else'],$row['diet_calory']));
+						  echo $row['diet_id'] ." 번식단 밥종류:" .$row['diet_grains'] ." 고기종류: ".$row['diet_meat']."채소 종류 ".$row['diet_vet']."기타: ".$row['diet_else']."칼로리 :".$row['diet_calory'];
+	  						echo nl2br("\n");
+  						}
 					} else {
- 						 echo "0 results";
+  					echo "0 results";
 					}
-				?>
+
+					for($i=0;$i<count($arr);$i++){
+						for($j=0;$j<6;$j++){	
+							echo $arr[$i][$j];
+							echo nl2br("\n");
+					}
+
+				}
+			?>
 				<form>
 					<select name="selectset" onchange="SetDisplay(this.form)" >
 						<option selected value=0>-선택하세요- </option>
