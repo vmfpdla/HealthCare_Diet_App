4c2735da00079bba029697980114c25e55f4c2ad
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2020-11-05 16:52:13 +0900

    Update lunch_diet.php

--- lunch_diet.php
+++ lunch_diet.php
@@ -1,70 +1,104 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title></title>
-  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <head>
+        <title></title>
+        <link
+            href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+            rel="stylesheet"
+            integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+            crossorigin="anonymous">
 
-  <link rel="stylesheet" href="/resource/css/bootstrap.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <!-- 폰트 -->
-  <link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Do+Hyeon&display=swap" rel="stylesheet">
-  <script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>
-  <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <meta charset="utf-8">
+        <link rel="stylesheet" href="/resource/css/bootstrap.css">
+        <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+            crossorigin="anonymous">
+        <!-- 폰트 -->
+        <link
+            href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Do+Hyeon&display=swap"
+            rel="stylesheet">
+        <script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+        <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+        <meta charset="utf-8">
 
+        <style type="text/css">
 
+            #container {
+                width: 960px;
+                margin: 0 auto;
+            }
+            #container #input-form {
+                text-align: center;
+            }
+            #user-table {
+                margin: 0 auto;
+                text-align: center;
+            }
+            #input-form {
+                margin-top: 10px;
+                margin-bottom: 10px;
+            }
+        </style>
 
-  <style type="text/css">
+        <script type="text/javascript">
+            $(document).ready(function () {
+                clickTd();
+            }
 
-    #container {width: 960px; margin: 0 auto;}
-    #container #input-form {text-align: center;}
-    #user-table {margin: 0 auto; text-align: center;}
-    #input-form {margin-top: 10px; margin-bottom: 10px;}
+            function clickTd() {
+                $("#user-table tr").click(function () {
+                    var text = $(this).text();
+                    alert('선택한 식단번호 : ' + text[0]);
+                    document.getElementById("dietnum").value = text[0];
+                });
 
+            }
+        </script>
 
-  </style>
+    </head>
 
-  <script type="text/javascript">   
+    <body>
 
-    
-    function clickTd(){
-      $("#user-table tr").click(function(){
-        var text = $(this).text();
-        alert('선택한 식단번호 : '+text[0]);
-        document.getElementById("foodnum").value=text[0];
-      });
+        <div id="container">
 
-    }
+            <div class="row row-cols-3">
+                <form method="post" action="lunch_diet2.php">
+                    <div class="input-group col">
 
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">식단 번호</span>
+                        </div>
+                        <input type="text" class="form-control" name="dietnum" id="dietnum" placeholder="Food Number" aria-label="Username" aria-describedby="basic-addon1">
 
-  </script>
+                        <button type="submit">
+                            제출
+                        </button>
+                    </form>
+                </div>
 
+                <table class="table table-hover" id="user-table">
+                    <thead>
+                        <tr>
+                            <th>식단번호</th>
+                            <th>곡류</th>
+                            <th>고기류</th>
+                            <th>채소류</th>
+                            <th>기타</th>
+                            <th>칼로리</th>
+                        </tr>
+                    </thead>
 
-</head>
-
-
-
-<body>
-
-  <div id="container">
-
-
-    <table class="table table-hover" id="user-table">
-      <thead>
-        <tr>
-          <th>식단번호</th>
-          <th>곡류</th>
-          <th>고기류</th>
-          <th>채소류</th>
-          <th>기타</th>
-          <th>칼로리</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        <?php
+                    <tbody>
+                    <?php
         #   header('Content-Type:text/html; charset=UTF-8');
 
 
@@ -137,14 +171,11 @@
         }
         
       ?>
-    </tbody>
-  </table>
-</div>
+                    </tbody>
+                </table>
+            </div>
 
-<form>
+            <form></form>
 
-
-</form>
-
-</body>
-</html>
+        </body>
+    </html>
