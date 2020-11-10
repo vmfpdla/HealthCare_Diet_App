f83e718b8f5408ff4761071c113bde8f40ffa18f
root <root@capstone.asia-northeast3-a.c.sogading.internal>
2020-11-10 04:30:16 +0000

    추가

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 require_once("./dbconn.php");
 
 $user_id = 1; # 1번 가져왔다고 가정
-$today = date("Y-m-d");
 $sql = "SELECT * FROM user WHERE user_id='$user_id'";
 $result = $conn->query($sql);
 
@@ -74,7 +73,7 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
 <body>
   <nav class="navbar fixed-top">
     <p class="navp">Smart PT</p>
-    <a href="usermodify.php">
+    <a href="userinsert.php">
       <i class="fa fa-user-circle navi"></i>
     </a>
 
@@ -87,7 +86,7 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
     <div class="card-body" style="text-align:center">
       <!-- 운동데이터를 가져오는 php 문 -->
       <?php
-      $sql1 = "SELECT * FROM doexercise INNER JOIN exerciseinfo on doexercise.exercise_id = exerciseInfo.exercise_id WHERE user_id='$user_id' and doexercise_day='$today'";
+      $sql1 = "SELECT * FROM doexercise INNER JOIN exerciseinfo on doexercise.exercise_id = exerciseInfo.exercise_id WHERE user_id='$user_id'";
       $result1 = $conn->query($sql1);
 
       if ($result1->num_rows > 0) { // 여러줄 가져오는 경우
@@ -98,7 +97,7 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
         echo nl2br("\n");
       }
       } else {
-        echo "아직 운동을 안했어요 !";
+        echo "0 results";
       }
     ?>
     </div>
@@ -106,47 +105,43 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
   <br><br>
 
 
-<a href="app://application">Call Android Activity</a>
+<a href="app://application">Call Android Activity</a> 
 <br><br>
   <div class="card">
     <div class="card-header">
       영양소
     </div>
     <?php
-      $today = date("Y-m-d");
-      $kcal = 0; //칼로리
-      $car =0; // 탄수화물
-      $fat =0; // 지방
-      $pro =0; // 단백질
+  $today = date("Y-m-d");
+  $kcal = 0; //칼로리
+  $car =0; // 탄수화물
+  $fat =0; // 지방
+  $pro =0; // 단백질
+  $sql2 = "SELECT * FROM eatenfood INNER JOIN foodinfo on eatenfood.food_id = foodinfo.food_id WHERE user_id='$user_id' and eaten_day='$today'";
+  $result2 = $conn->query($sql2);
 
-      $maxcar = $user['user_goal'] * 0.65;
-      $maxfat =$user['user_goal'] * 0.2;
-      $maxpro =$user['user_goal'] * 0.15;
-      $sql2 = "SELECT * FROM eatenfood INNER JOIN foodinfo on eatenfood.food_id = foodinfo.food_id WHERE user_id='$user_id' and eaten_day='$today'";
-      $result2 = $conn->query($sql2);
+  if ($result2 -> num_rows>0) { // 여러줄 가져오는 경우
 
-      if ($result2 -> num_rows>0) { // 여러줄 가져오는 경우
+  while($row = $result2->fetch_assoc()) {
+    if($row['eaten_serving']==0){ # 0인분인경우
+      $kcal = $kcal + $row['food_calory']*0.5;
+      $car = $car + $row['food_car']*0.5;
+      $fat = $fat + $row['food_fat']*0.5;
+      $pro = $pro + $row['food_pro']*0.5;
+   }
+   else{
+    $kcal = $kcal + $row['food_calory']*$row['eaten_serving'];
+    $car = $car + $row['food_car']*$row['eaten_serving'];
+    $fat = $fat + $row['food_fat']*$row['eaten_serving'];
+    $pro = $pro + $row['food_pro']*$row['eaten_serving'];
+   }
+  #echo $row['exercise_name'] ." / " .$row['exercise_minute'] ."분 / ".$row['exhausted_calory']."Kcal";
+  #echo nl2br("\n");
+  }
 
-      while($row = $result2->fetch_assoc()) {
-        if($row['eaten_serving']==0){ # 0인분인경우
-          $kcal = $kcal + $row['food_calory']*0.5;
-          $car = $car + $row['food_car']*0.5;
-          $fat = $fat + $row['food_fat']*0.5;
-          $pro = $pro + $row['food_pro']*0.5;
-       }
-       else{
-        $kcal = $kcal + $row['food_calory']*$row['eaten_serving'];
-        $car = $car + $row['food_car']*$row['eaten_serving'];
-        $fat = $fat + $row['food_fat']*$row['eaten_serving'];
-        $pro = $pro + $row['food_pro']*$row['eaten_serving'];
-       }
-      #echo $row['exercise_name'] ." / " .$row['exercise_minute'] ."분 / ".$row['exhausted_calory']."Kcal";
-      #echo nl2br("\n");
-      }
-
-      }
-      else //echo "0 results";
-    ?>
+  }
+  else //echo "0 results";
+?>
     <div class="card-body">
 
       <div class="container">
@@ -157,7 +152,7 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
 
         <div class="progress rounded-pill" style="height:40px;">
           <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar"
-            style="width: <?php echo $kcal/ $user['user_goal']*100;?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
+            style="width: <?php echo $kcal/1500*100;?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
             <p class="pr" style="padding-top:15px;">
               <?php echo $kcal ." / ". $user['user_goal'] ?>
             </p>
@@ -175,9 +170,9 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
 
         <div class="progress rounded-pill" style="height:30px;">
           <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
-            style="width: <?php echo $car/$maxcar*100;?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
+            style="width: <?php echo $car/1500*100;?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
             <p class="pr" style=" padding-top:20px;">
-              <?php echo $car ." / ". $maxcar ?>
+              <?php echo $car ." / ". $user['user_goal'] ?>
             </p>
           </div>
         </div>
@@ -195,9 +190,9 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
 
         <div class="progress rounded-pill" style="height:30px;">
           <div class="progress-bar progress-bar-striped bg-success progress-bar-animated" role="progressbar"
-            style="width: <?php echo $pro/$maxpro*100;?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
+            style="width: <?php echo $pro/1500*100;?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
             <p class="pr" style=" padding-top:20px;">
-              <?php echo $pro ." / ". $maxpro?>
+              <?php echo $pro ." / ". $user['user_goal'] ?>
             </p>
           </div>
         </div>
@@ -214,9 +209,9 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
 
         <div class="progress rounded-pill" style="height:30px;">
           <div class="progress-bar progress-bar-striped bg-info progress-bar-animated" role="progressbar"
-            style="width: <?php echo $fat/$maxfat*100;?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
+            style="width: <?php echo $fat/1500*100;?>%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
             <p class="pr" style="padding-top:20px;">
-              <?php echo $fat ." / ". $maxfat ?>
+              <?php echo $fat ." / ". $user['user_goal'] ?>
             </p>
           </div>
         </div>
@@ -285,39 +280,37 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
     </div>
     <div class="card-body" id="diet_1">
       <div style="float:left; margin:50px 50px; width:15%;">
-        <p style="width:100%">
-          <?php
-            if($is_morning==1){
-              $morning_kcal =0;
-              $morning_car =0;
-              $morning_pro =0;
-              $morning_fat =0;
+        <p style="width:100%"><?php
+      if($is_morning==1){
+        $morning_kcal =0;
+        $morning_car =0;
+        $morning_pro =0;
+        $morning_fat =0;
 
-            	while($row = $result3->fetch_assoc()) {
-                if ($row['eaten_serving']==0){
-                     $morning_kcal = $morning_kcal+$row['food_calory']*0.5;
-                     $morning_car = $morning_car+$row['food_car']*0.5;
-                     $morning_pro = $morning_pro+$row['food_pro']*0.5;
-                     $morning_fat = $morning_fat+$row['food_fat']*0.5;
-               }
-               else{
-                     $morning_kcal = $morning_kcal+$row['food_calory']*$row['eaten_serving'];
-                     $morning_car = $morning_car+$row['food_car']*$row['eaten_serving'];
-                     $morning_pro = $morning_pro+$row['food_pro']*$row['eaten_serving'];
-                     $morning_fat = $morning_fat+$row['food_fat']*$row['eaten_serving'];
-               }
-                if($row['eaten_serving']==0){
-                  echo $row['food_name'] ."  ". $row['food_calory']*$row['eaten_serving']."  Kcal"."<br>(0.5 인분)";
-          	      echo nl2br("\n\n");
-                }
-                else{
-                  echo $row['food_name'] ."  ". $row['food_calory']*$row['eaten_serving']."  Kcal"."<br>(".$row['eaten_serving']." 인분)";
-          	      echo nl2br("\n\n");
-               	}
-              }
-          }
-          ?>
-        </p>
+  	while($row = $result3->fetch_assoc()) {
+      if ($row['eaten_serving']==0){
+           $morning_kcal = $morning_kcal+$row['food_calory']*0.5;
+           $morning_car = $morning_car+$row['food_car']*0.5;
+           $morning_pro = $morning_pro+$row['food_pro']*0.5;
+           $morning_fat = $morning_fat+$row['food_fat']*0.5;
+     }
+     else{
+           $morning_kcal = $morning_kcal+$row['food_calory']*$row['eaten_serving'];
+           $morning_car = $morning_car+$row['food_car']*$row['eaten_serving'];
+           $morning_pro = $morning_pro+$row['food_pro']*$row['eaten_serving'];
+           $morning_fat = $morning_fat+$row['food_fat']*$row['eaten_serving'];
+     }
+      if($row['eaten_serving']==0){
+          echo $row['food_name'] ."  ". $row['food_calory']*$row['eaten_serving']."  Kcal"."<br>(0.5 인분)";
+	  echo nl2br("\n\n");
+      }
+      else{
+          echo $row['food_name'] ."  ". $row['food_calory']*$row['eaten_serving']."  Kcal"."<br>(".$row['eaten_serving']." 인분)";
+	  echo nl2br("\n\n");
+     	 }
+        }
+      }
+    ?></p>
       </div>
 
       <div style="margin:20px 30px; float:left; width:60%;" class="smallbar">
@@ -675,8 +668,8 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
     var is_dinner = '<?php echo $is_dinner ?>';
 
     if (is_morning == 1) {
+
       document.getElementById('diet_1').style.display = 'block';
-      document.getElementById('camera_1').style.display = 'none';
     }
     else {
 
@@ -687,7 +680,6 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
     if (is_lunch == 1) {
 
       document.getElementById('diet_2').style.display = 'block';
-      document.getElementById('camera_2').style.display = 'none';
     }
     else {
 
@@ -698,7 +690,6 @@ if ($result->num_rows > 0) { // 여러줄 가져오는 경우
     if (is_dinner == 1) {
 
       document.getElementById('diet_3').style.display = 'block';
-      document.getElementById('camera_3').style.display = 'none';
     }
     else {
 
