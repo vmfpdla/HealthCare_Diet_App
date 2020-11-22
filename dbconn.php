fc73f8d5b18d6ce07f50e8615df348dc1645edd8
vmfpdla <vmfpdla0@naver.com>
2020-11-22 21:03:26 +0900

    db

--- dbconn.php
+++ dbconn.php
@@ -4,8 +4,8 @@ error_reporting(0);
 
   $host = 'localhost';
   $db_user = 'root';
-  $db_pw = 'ghkdduswn123'; #toor
-  $db_name = 'toor';
+  $db_pw = 'toor'; #toor
+  $db_name = 'smartpt';
   $conn = new mysqli($host, $db_user, $db_pw, $db_name);
 
 
