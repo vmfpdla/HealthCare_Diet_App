7c01ae03635a9b03ec62b03b236cde5019d08e0a
vmfpdla <vmfpdla0@naver.com>
2020-11-27 14:36:26 +0900

    modify

--- usermodify.php
+++ usermodify.php
@@ -89,7 +89,7 @@
 				</div>
 				<br><br>
 				<div class="form-group col-md-13">
-					<label style="font-size:60px;">하루 섭취칼로리 설정</label>
+					<label style="font-size:60px; float:left;">하루 섭취칼로리 설정</label>
 					<div class="btn-group btn-group-toggle" data-toggle="buttons" style="margin-left:30px; margin-top:-30px;">
 						<button class="btn btn-outline-secondary" style="font-size:30px;" type="button" name="calory_type" value="권장칼로리" onclick="changeCalory(this.value)">권장칼로리</button>
 						<button class="btn btn-outline-secondary" style="font-size:30px;" type="button" name="calory_type" value="직접 입력" onclick="changeCalory(this.value)"/> 직접 입력</button>
